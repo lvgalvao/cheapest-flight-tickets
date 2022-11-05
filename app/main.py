e27abbaea3df589041dc32f8c76d3f4c7cf8873f
Luciano Filho <lvgalvaofilho@gmail.com>
2022-11-05 15:18:28 -0300

    app/main.py

--- app/main.py
+++ app/main.py
@@ -6,17 +6,17 @@ import pprint
 dataManager = DataManager()
 flightSearch = FlightSearch()
 sheet_data = dataManager.get_sheety_data()
-# sheet_data = [
-#     {"city": "Paris", "iataCode": "", "lowestPrice": 54, "id": 2},
-#     {"city": "Berlin", "iataCode": "", "lowestPrice": 42, "id": 3},
-#     {"city": "Tokyo", "iataCode": "", "lowestPrice": 485, "id": 4},
-#     {"city": "Sydney", "iataCode": "", "lowestPrice": 551, "id": 5},
-#     {"city": "Istanbul", "iataCode": "", "lowestPrice": 95, "id": 6},
-#     {"city": "Kuala Lumpur", "iataCode": "", "lowestPrice": 414, "id": 7},
-#     {"city": "New York", "iataCode": "", "lowestPrice": 240, "id": 8},
-#     {"city": "San Francisco", "iataCode": "", "lowestPrice": 260, "id": 9},
-#     {"city": "Cape Town", "iataCode": "", "lowestPrice": 378, "id": 10},
-# ]
+    # sheet_data = [
+    #     {"city": "Paris", "iataCode": "", "lowestPrice": 54, "id": 2},
+    #     {"city": "Berlin", "iataCode": "", "lowestPrice": 42, "id": 3},
+    #     {"city": "Tokyo", "iataCode": "", "lowestPrice": 485, "id": 4},
+    #     {"city": "Sydney", "iataCode": "", "lowestPrice": 551, "id": 5},
+    #     {"city": "Istanbul", "iataCode": "", "lowestPrice": 95, "id": 6},
+    #     {"city": "Kuala Lumpur", "iataCode": "", "lowestPrice": 414, "id": 7},
+    #     {"city": "New York", "iataCode": "", "lowestPrice": 240, "id": 8},
+    #     {"city": "San Francisco", "iataCode": "", "lowestPrice": 260, "id": 9},
+    #     {"city": "Cape Town", "iataCode": "", "lowestPrice": 378, "id": 10},
+    # ]
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -24,10 +24,12 @@ if sheet_data[0]["iataCode"] == "":
     cities = []
     for rows in range(len(sheet_data)):
         cities.append(sheet_data[rows]["city"])
-    # print(cities)
+        # print(cities)
     iata_code = flightSearch.get_iata_code(cities)
     for rows in range(len(sheet_data)):
         sheet_data[rows]["iataCode"] = iata_code[rows]
     new_sheet_data = dataManager.post_sheety_data(datasheet=sheet_data)
-    pp.pprint(new_sheet_data)
+    # pp.pprint(new_sheet_data)
 
+fs = FlightSearch()
+fs.get_cheap_flights(sheet_data)
