ff22a88d53c5aa012e2469162c4f110b2030d0f2
Luciano Filho <lvgalvaofilho@gmail.com>
2022-11-06 11:38:54 -0300

    fix

--- app/main.py
+++ app/main.py
@@ -5,18 +5,18 @@ import pprint
 
 dataManager = DataManager()
 flightSearch = FlightSearch()
-sheet_data = dataManager.get_sheety_data()
-    # sheet_data = [
-    #     {"city": "Paris", "iataCode": "", "lowestPrice": 54, "id": 2},
-    #     {"city": "Berlin", "iataCode": "", "lowestPrice": 42, "id": 3},
-    #     {"city": "Tokyo", "iataCode": "", "lowestPrice": 485, "id": 4},
-    #     {"city": "Sydney", "iataCode": "", "lowestPrice": 551, "id": 5},
-    #     {"city": "Istanbul", "iataCode": "", "lowestPrice": 95, "id": 6},
-    #     {"city": "Kuala Lumpur", "iataCode": "", "lowestPrice": 414, "id": 7},
-    #     {"city": "New York", "iataCode": "", "lowestPrice": 240, "id": 8},
-    #     {"city": "San Francisco", "iataCode": "", "lowestPrice": 260, "id": 9},
-    #     {"city": "Cape Town", "iataCode": "", "lowestPrice": 378, "id": 10},
-    # ]
+# sheet_data = dataManager.get_sheety_data()
+sheet_data = [
+    {"city": "Paris", "iataCode": "CDG", "lowestPrice": 54, "id": 2},
+    {"city": "Berlin", "iataCode": "BER", "lowestPrice": 42, "id": 3},
+    {"city": "Tokyo", "iataCode": "NRT", "lowestPrice": 485, "id": 4},
+    {"city": "Sydney", "iataCode": "SYD", "lowestPrice": 551, "id": 5},
+    {"city": "Istanbul", "iataCode": "SAW", "lowestPrice": 95, "id": 6},
+    {"city": "Kuala Lumpur", "iataCode": "KUL", "lowestPrice": 414, "id": 7},
+    {"city": "New York", "iataCode": "JFK", "lowestPrice": 240, "id": 8},
+    {"city": "San Francisco", "iataCode": "SFO", "lowestPrice": 260, "id": 9},
+    {"city": "Cape Town", "iataCode": "CPT", "lowestPrice": 378, "id": 10},
+]
 
 pp = pprint.PrettyPrinter(indent=4)
 
